8b4686fe29f109c0b0ad7c9c1606b788ab0a62cc
Muhammad Bilal Aleem <30667628+brad911@users.noreply.github.com>
2025-10-21 19:55:30 +0500

    change id pw on authlogn

--- dist/assets/index-DdbdGWFl.js
+++ dist/assets/index-DdbdGWFl.js
@@ -1,4 +1,4 @@
-import{aw as di,ak as mi,r as s,P as d,af as ui}from"./index-DgXo-GRP.js";/**
+import{aw as di,ak as mi,r as s,P as d,af as ui}from"./index-ilolb5oh.js";/**
  * @license @tabler/icons-react v3.31.0 - MIT
  *
  * This source code is licensed under the MIT license.
