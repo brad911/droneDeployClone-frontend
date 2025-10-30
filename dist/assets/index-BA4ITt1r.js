217dd2432b386de6aee18805413543bc9ada1096
Muhammad Bilal Aleem <30667628+brad911@users.noreply.github.com>
2025-10-30 18:37:52 +0500

    remove id pw

--- dist/assets/index-BA4ITt1r.js
+++ dist/assets/index-BA4ITt1r.js
@@ -1,4 +1,4 @@
-import{aw as di,ak as mi,r as s,P as d,af as ui}from"./index-wjFNmjwi.js";/**
+import{aw as di,ak as mi,r as s,P as d,af as ui}from"./index-DwhPnNYo.js";/**
  * @license @tabler/icons-react v3.31.0 - MIT
  *
  * This source code is licensed under the MIT license.
